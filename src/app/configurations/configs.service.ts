4f17963c7a548e064ee8dc32d40765a9b1415081
Santos92 <AlexanderLundbergSantos@live.se>
2019-05-07 00:14:14 +0200

    retry

--- src/app/configurations/configs.service.ts
+++ src/app/configurations/configs.service.ts
@@ -28,7 +28,7 @@ export class ConfigsService{
   tenant;
   config : configs;
   token : token;
-  frontendConfigServiceUrl = 'http://gwr-frontend-configurator.demo1.svc.cluster.local/configs';
+  frontendConfigServiceUrl = 'http://gwr-frontend-configurator/configs';
 
   constructor(private injector: Injector) { }
   ngOnInit(){
